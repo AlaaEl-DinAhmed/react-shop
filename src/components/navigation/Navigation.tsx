669f55d24b75fdf1f37d501a56244afdc6fa953b
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-09-01 00:34:23 +0200

    Adding navigation styles

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,22 +1,30 @@
+import { ReactComponent as Logo } from "@/assets/images/shopify-seeklogo.com.svg";
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
+import styles from "./Navigation.module.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
-      <nav>
-        <ul>
+      <nav className={styles["navigation-container"]}>
+        <Link to="/">
+          <Logo />
+        </Link>
+        <ul className={styles["navigation-links"]}>
           <li>
-            <Link to="/">Logo</Link>
+            <Link className={styles["navigation-links__link"]} to="/shop">
+              Shop
+            </Link>
           </li>
           <li>
-            <Link to="/shop">Shop</Link>
+            <Link className={styles["navigation-links__link"]} to="/contact">
+              Contact
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/shop">Sign in</Link>
+            <Link className={styles["navigation-links__link"]} to="/shop">
+              Sign In
+            </Link>
           </li>
         </ul>
       </nav>
